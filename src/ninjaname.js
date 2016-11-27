8d1c83f9a084461d06247fb4680437beb2e251d0
vandrewskis <vandrewskis@users.noreply.github.com>
2016-11-27 12:08:30 +0100

    handle other - non ninja - characters
    copy non ninja characters

--- src/ninjaname.js
+++ src/ninjaname.js
@@ -1,25 +1,25 @@
 var ninjacode = ['ki', 'lu', 'chi', 'zu', 'ke', 'ru', 'ji', 'to', 'me', 'zi', 'shi', 'no', 'ta', 'chi', 'zu', 'ta', 'ari', 'rin', 'no', 'shi', 'ga', 'mei', 'ku', 'te', 'fu', 'yo'];
 
 function ninjaname(name) {
-    var ninjaname = "";
+  var ninjaname = "";
 
-    for (i = 0; i < name.length; i++) {
-        var n = name.charCodeAt(i);
-        // handle space
-        if (n == 32) {
-            ninjaname += " ";
-        }
-        // handle UPPER CASE
-        else if (n >= 65 && n < 91) {
-            var code = ninjacode[n - 65];
-            code = code.charAt(0).toUpperCase() + code.slice(1);
-            ninjaname += code;
-        }
-        // handle lower case
-        else if (n >= 97 && n < 123) {
-            ninjaname += ninjacode[n - 97];
-        }
+  for (i = 0; i < name.length; i++) {
+    var n = name.charCodeAt(i);
+    // handle UPPER CASE
+    if (n >= 65 && n < 91) {
+      var code = ninjacode[n - 65];
+      code = code.charAt(0).toUpperCase() + code.slice(1);
+      ninjaname += code;
     }
+    // handle lower case
+    else if (n >= 97 && n < 123) {
+      ninjaname += ninjacode[n - 97];
+    }
+    // handle rest
+    else {
+      ninjaname += name[i];
+    }
+  }
 
-    return ninjaname;
+  return ninjaname;
 }
